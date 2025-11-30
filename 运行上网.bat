900327c70774a27f66a3a3082445c382047a4bb1
GSlege <liaoshuheng@gmail.com>
2025-11-30 13:38:03 +0800

    Add files via upload

---
+++ 运行上网.bat
@@ -0,0 +1,11 @@
+@echo off
+title ech-workers Client
+
+cd d:\科学上网Worker\ech客户端及js
+   ech.exe^
+  -f ech.lege.asia:443^
+  -l 127.0.0.1:8080^
+  -pyip proxyip.cmliussss.net^
+  -token gslege^
+  -ip ech.lege.asia
+pause
